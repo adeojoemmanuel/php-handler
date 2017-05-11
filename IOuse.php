a10855716c3269e00caeae8962275c47129e847a
adeojo emmanuel <emmanuel.adeojo@yahoo.com>
2017-05-11 20:31:56 +0100

    Update IOuse.php

--- IOuse.php
+++ IOuse.php
@@ -3,4 +3,14 @@
     $class .".php";
   }
   load('IOchander');
+  
+  $class = new IOhandler;
 ?>
+<!doctype html>
+<html>
+  <head>
+    <title>your title</title>
+  </head>
+  <body>
+  </body>
+</html>
