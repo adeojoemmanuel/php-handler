8089cb036806785380fd299eca98c6f724d540ad
adeojo emmanuel <emmanuel.adeojo@yahoo.com>
2017-05-11 20:37:16 +0100

    Update IOuse.php

--- IOuse.php
+++ IOuse.php
@@ -12,5 +12,18 @@
     <title>your title</title>
   </head>
   <body>
+    // for get all user from database
+    <?php 
+      $query = $class->getAll('your_table_name');
+      
+      echo $query;
+    ?>
+    
+    //for get single id 
+    
+    $query = $class->getBy_id(your_id, table);
+    
+    echo $query;
+    
   </body>
 </html>
