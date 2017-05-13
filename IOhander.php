899f8e6feff42bcbe54dcc8846f2803dfb5e7466
DESKTOP-TL741B2\IM <emmanuel.adeojo@yahoo.com>
2017-05-13 20:03:51 +0100

    added login and a function to include

--- IOhander.php
+++ IOhander.php
@@ -1,13 +1,11 @@
 <?php 
 include('config.php');
-	
 	class IOhandler{	
 		public function __construct(){
 	 		include('dbconfig.php');
 	 		$db = new connect();
         	$this->DBcon = $db->startConn();
 		}
-
 		public function getAll($table){
 			$SQL = "SELECT * from $table";
 			$q = $this->DBcon->query($SQL) or die("Failed");
@@ -16,7 +14,6 @@ include('config.php');
 			}
 			return $data;
 		}
-
 		public function getBy_id($id, $table){
 			$SQL = "SELECT * from $table where _id = :id";
 			$q = $this->DBcon->prepare($SQL);
@@ -24,21 +21,26 @@ include('config.php');
 			$data = $q->FETCH_ASSOC();
 			return $data;
 		}
+		public function insert($table, array $fields, array $values) {
+		    $numFields = count($fields);
+		    $numValues = count($values);
+		    if($numFields === 0 or $numValues === 0)
+		        throw new Exception("At least one field and value is required.");
+		    if($numFields !== $numValues)
+		        throw new Exception("Mismatched number of field and value arguments.");
 
-		public function insert($table, $fields = array(), $values = array()) {
-		    $fields = '`' . implode ( '`,`', $fields ) . '`';
-		    $values = "'" . implode ( "','", $values ) . "'";
+		    $fields = '`' . implode('`,`', $fields) . '`';
+		    $values = "'" . implode("','", $values) . "'";
 		    $sql = "INSERT INTO {$table} ($fields) VALUES($values)";
-
-		    if ($q=$this->DBcon->prepare ( $sql )) {
+			
+			if ($q=$this->DBcon->prepare ( $sql )) {
+		       // echo json_encode($sql);
 		        if ($q->execute()) {
 		            return true;
 		        }
 		    }
 		    return false;
 		}
-
-		
 		public function update($table,$values=array(),$where){
             $args=array();
 			foreach($values as $field=>$value){
@@ -53,14 +55,12 @@ include('config.php');
    			}
    			return false;
     	}
-		
 		public function deleteData($id, $table){
 			$SQL = "DELETE from $table where _id = :id";
 			$q = $this->DBcon->prepare($SQL);
 			$q->execute(array(':id' => $id));
 			return true;
 		}
-
 		public function startSession(){
 			if (!isset($_SESSION['id'])) {
 				session_start();
@@ -69,7 +69,6 @@ include('config.php');
 				$sessid = $_SESSION['id'];
 			}	
 		}
-
 		public function endSession(){
 			if(!isset($_SESSION['id'])){
 				session_start();
@@ -78,7 +77,6 @@ include('config.php');
 	    		session_destroy();  
 			}
 	    }
-
 	    public function sendMail($values = array()){
 	    	$values = '`' . implode ( '`,`', $values ) . '`';
 		    $mail_status = mail($values);
@@ -88,7 +86,6 @@ include('config.php');
 		        return false;
 		    }
 	    }
-
 	    public function checkTableExist($table){
 	    	$sql = "'SHOW TABLES FROM '.$this->dbname.' LIKE '.$table.''";
 	    	if($sql){
@@ -99,7 +96,28 @@ include('config.php');
 	            }
 	        }
 	    }
-	}	
-
-	
+	    public function validateInput($input){
+			$input=preg_replace("#[^0-9a-z]#i","",$input);
+	    }
+	    
+	    public function login($username, $password, $table, $dbtable){
+		    $query = $DBcon->prepare("SELECT * FROM $table WHERE $dbtable='$username' ORDER BY _userid DESC limit 1");
+		    $vee=$query->execute();
+		    $row=$vee->fetch_array();
+		    $count=$query->num_rows;
+		    $getpw = $row['password'];
+		    $verify = password_verify($password, $getpw);
+		    if(($count)){
+		        if ($verify) {
+		           $_SESSION['userid'] = $row['_userid'];
+		            echo "ok";
+		        } else {
+		            echo "incorrect password";
+		        }
+		    } else {
+		         echo  "email not exist try logging in with your username";
+		    }
+			$DBcon->close();
+		}
+	}		
 ?>
